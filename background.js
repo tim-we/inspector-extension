dfb0efb895202f9c6087e0b79a22bb11a1bfa46d
Tim Weißenfels <tim-we@users.noreply.github.com>
2021-06-01 23:46:58 +0200

    Improve URL parser

--- background.js
+++ background.js
@@ -1,9 +1,17 @@
 browser.pageAction.onClicked.addListener((tab) => {
     const url = new URL(tab.url);
-    const extension = url.pathname.replace(/\/+$/, "").split("/").pop();
+
+    // parse URL (get extension slug)
+    const parts = url.pathname.split("/");
+    const n = parts.length;
+    const i = parts.findIndex(
+        (v, i) => v === "firefox" && i + 1 < n && parts[i + 1] === "addon"
+    );
+    const extension = parts[i + 2];
+
     browser.tabs.create({
         active: true,
         openerTabId: tab.id,
-        url: `https://tim-we.github.io/web-ext-inspector/?extension=${extension}`
+        url: `https://tim-we.github.io/web-ext-inspector/?extension=${extension}`,
     });
 });
